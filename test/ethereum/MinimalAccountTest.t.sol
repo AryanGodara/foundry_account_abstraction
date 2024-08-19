669b2fbfcd1dd023ec2103e0295aaa42f06cc27b
Aryan Godara <aryangodara03@gmail.com>
2024-08-19 10:32:13 +0530

    add test to check entryPointOrOwner modifiers working properly

--- test/ethereum/MinimalAccountTest.t.sol
+++ test/ethereum/MinimalAccountTest.t.sol
@@ -14,6 +14,8 @@ contract MinimalAccountTest is Test {
     MinimalAccount private minimalAccount;
     ERC20Mock private usdc = new ERC20Mock();
 
+    address private randomUser = makeAddr("randomUser");
+
     uint256 private constant AMOUNT = 1e18;
 
     function setUp() public {
@@ -48,5 +50,26 @@ contract MinimalAccountTest is Test {
         assertEq(
             usdc.balanceOf(address(minimalAccount)), AMOUNT
         );
+    }
+
+    function testNonOwnerCannotExecuteCommands() public {
+        // Arrange
+        assertEq(
+            usdc.balanceOf(address(minimalAccount)), 0
+        );
+
+        address dest = address(usdc);
+        uint256 value = 0;
+        bytes memory functionData =
+                            abi.encodeWithSelector(
+                ERC20Mock.mint.selector,
+                address(minimalAccount),
+                AMOUNT
+            );
+
+        // Act and Assert
+        vm.prank(randomUser);
+        vm.expectRevert(MinimalAccount.MinimalAccount_NotFromEntryPointOrOwner.selector);
+        minimalAccount.execute(dest, value, functionData);
     }
 }
